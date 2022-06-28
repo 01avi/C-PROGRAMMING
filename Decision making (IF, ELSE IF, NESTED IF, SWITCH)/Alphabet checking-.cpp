f75df93a82071d10422fedf2b68921c5dc888fc6
Aviral <107908970+01avi@users.noreply.github.com>
2022-06-28 10:17:57 +0530

    Check Alphabet

--- Decision making (IF, ELSE IF, NESTED IF, SWITCH)/Alphabet checking-.cpp
+++ Decision making (IF, ELSE IF, NESTED IF, SWITCH)/Alphabet checking-.cpp
@@ -6,10 +6,10 @@ main()
 	scanf("%c", &ch);
 	if((ch=='a')||(ch=='e')||(ch=='i')||(ch=='o')||(ch=='u')||(ch=='A')||(ch=='E')||(ch=='I')||(ch=='O')||(ch=='U'))
 	{
-		printf("character is vowel");
+		printf("character is VOWEL");
 	}
 	else
 	{
-		printf("Character is consonant");
+		printf("Character is CONSONANT");
 	}
 }
