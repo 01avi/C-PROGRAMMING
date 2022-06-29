78d368b788f45772d300db4609e4766f41a77511
Aviral <107908970+01avi@users.noreply.github.com>
2022-06-29 09:57:38 +0530

    Add files via upload

---
+++ Area of circle.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+	 float p=3.14;
+	 float R,A;
+  
+	 printf("PLEASE ENTER THE VALUE OF THE RADIOUS OF CIRCLE:- ");
+	 scanf("%f", &R);
+  
+	 A= p*R*R;
+  
+	 printf("AREA OF THE CIRCLE IS:- %f", A);
+  
+ }
+	
