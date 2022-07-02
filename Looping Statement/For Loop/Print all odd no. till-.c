ae3aa9b9a44dee6e657f240fd4a230b57346aea1
Aviral <107908970+01avi@users.noreply.github.com>
2022-07-02 11:17:23 +0530

    for loop

--- Looping Statement/For Loop/Print all odd no. till-.c
+++ Looping Statement/For Loop/Print all odd no. till-.c
@@ -2,9 +2,9 @@
 main()
 {
 	int i,n;
-	printf("PRINT ALL ODD NO TILL:--");
+	printf("PRINT ALL ODD NO TILL:-");
 	scanf("%d", &n);
-	printf("ALL ODD NUMBER FROM 1 TO %d ARE:--\n",n);
+	printf("ALL ODD NUMBER FROM 1 TO %d ARE:- \n",n);
 	for(i=1; i<=n; i++)
 	{
 		if(i%2!=0)
