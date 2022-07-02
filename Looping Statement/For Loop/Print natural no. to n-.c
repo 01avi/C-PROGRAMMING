9fc09285793e39517866ebfd415f411d235c621d
Aviral <107908970+01avi@users.noreply.github.com>
2022-07-02 11:11:18 +0530

    for loop

---
+++ Looping Statement/For Loop/Print natural no. to n-.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main()
+{
+	int i,n;
+	printf("ENTER ANY NUMBER:--");
+	scanf("%d",&n);
+	printf("NATURAL NUMBERS FROM 1 TO %d:\n",n);
+	for(i=1;i<=n;i++)
+		{
+			printf("%d\n",i);
+        }
+}
+		
