d5e9672a8c1f6a3c2c3e50eb1d94b2cd2752f907
Aviral <107908970+01avi@users.noreply.github.com>
2022-07-08 10:33:29 +0530

    If else

--- Decision making (IF, ELSE IF, NESTED IF, SWITCH)/To find the validity of triangle-.c
+++ Decision making (IF, ELSE IF, NESTED IF, SWITCH)/To find the validity of triangle-.c
@@ -2,7 +2,7 @@
 main()
 {
 	int Angle1, Angle2, Angle3, SUM;
-	printf("Enter Angles of triangle=\n");
+	printf("Enter Angles of triangle:- \n");
 	scanf("%d%d%d", &Angle1, &Angle2, &Angle3);
 	SUM=Angle1+ Angle2+ Angle3;
 	if(SUM==180 &&Angle1>0 &&Angle2>0 &&Angle3>0)
