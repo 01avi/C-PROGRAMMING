be4412f193945e981bfa19b65f820bdbbbc695ff
Aviral <107908970+01avi@users.noreply.github.com>
2022-07-09 11:06:05 +0530

    Switch

---
+++ Switch case/Find vowel or consonant-.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+main()
+{
+	char ch;
+	printf("TYPE ANY ALPHABET:-");
+	scanf("%c",&ch);
+	switch(ch)
+	{
+	   case 'a':
+		printf("VOWEL");
+		break;
+		case 'e':
+			printf("VOWEL");
+			break;
+			case 'i':
+				printf("VOWEL");
+				break;
+				case 'o':
+					printf("VOWEL");
+					break;
+					case 'u':
+						printf("VOWEL");
+						break;
+						case 'A':
+							printf("VOWEL");
+							break;
+								case 'E':
+									printf("VOWEL");
+									break;
+									case 'I':
+										printf("VOWEL");
+										break;
+										case 'O':
+											printf("vowel");
+											break;
+											case 'U':
+												printf("VOWEL");
+												break;
+												default:
+													printf("CONSONANT");
+													break;
+									
+				
+	}
+
+}
