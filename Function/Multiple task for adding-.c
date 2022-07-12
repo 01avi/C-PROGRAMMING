16b76200dd43bc7af8df3a09c8e914e8622a6dc3
Aviral <107908970+01avi@users.noreply.github.com>
2022-07-12 10:29:19 +0530

    NO RETURN NO ARGUMENT

---
+++ Function/Multiple task for adding-.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void sum()
+{
+	float a,b,sum=0;
+	printf("ENTER TWO NUMBERS;-");
+	scanf("%f%f",&a,&b);
+	sum=a+b;
+	printf("ADDITION OF THE NUMBERS IS= %f\n",sum);
+}
+void main()
+{
+	sum();
+	sum();
+	sum();
+	sum();
+	sum();
+	sum();
+}
+
+
+
+
+
+	
+	
+
