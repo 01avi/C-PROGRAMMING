db80fec070eb00e213c0ec068ec0d4822324e64f
Aviral <107908970+01avi@users.noreply.github.com>
2022-07-13 09:46:53 +0530

    NO RETURN NO ARGUMENT

---
+++ Function/Sum of two integer-.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void add()
+{
+	int x, y,z;
+	printf("ENTER TWO NUMBERS:-");
+	scanf("%d%d",&x,&y);
+	z= x+y;
+	printf("sum of two numbers is:-%d",z);
+}
+void main()
+{
+	add();
+}
