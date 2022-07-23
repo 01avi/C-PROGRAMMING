a749b8b931e5ecf7e2690885bdfa1753d2ac44d2
Aviral <107908970+01avi@users.noreply.github.com>
2022-07-23 09:56:34 +0530

    Reverse triangle

---
+++ 0. Print Designs/Print reverse triangle.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main()
+	{
+		int i, j, n;
+		printf("Enter Triangle lenght:- ");
+		scanf("%d", &n);
+	    for(i=n;i>=1;i--){
+             for(j=1;j<=i;j++){
+                 printf("*");
+           }
+            printf("\n");
+        } 
+}
